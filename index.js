13c136030f0cfa8e5a6b6d846f8f1577898b9e20
Tasin Ishmam <tasinishmam@gmail.com>
2019-06-03 10:45:32 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const expressEdge = require('express-edge');
+
+const app = new express();
+
+let port = 3000;
+
+app.use(express.static('public'));
+app.use(expressEdge);
+app.set('views', __dirname + '/views');
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
