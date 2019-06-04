56303e9868c4a14fda6478dd5819bb236cd49d19
Tasin Ishmam <tasinishmam@gmail.com>
2019-06-04 16:05:34 +0600

    Created posts viewing route and displaying all previous posts in index page

---
+++ database/models/post.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const expressEdge = require('express-edge');
 var {mongoose} = require('./database/mongoose');
+const bodyParser = require('body-parser');
+const Post = require('./database/models/post');
 
 
 const app = new express();
@@ -10,23 +12,43 @@ const app = new express();
 
 app.use(express.static('public'));
 app.use(expressEdge);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('views', __dirname + '/views');
 
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+
+    const posts = await Post.find({});
+    res.render('index', {
+        posts
+    });
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+
+app.get('/post/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('post', {
+        post
+    })
 });
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+
+app.get('/posts/new', (req, res) => {
+    res.render('create');
 });
 
-app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+app.post('/posts/store', async (req, res) => {
+
+    try {
+        let postresult = await Post.create(req.body);
+        res.redirect('/')
+    } catch (e) {
+        console.log(e);
+        res.status(400).send();
+    }
 });
 
 app.listen(process.env.PORT, () => {
